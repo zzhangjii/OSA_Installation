22becabaa65bd9a2afa4f2c2f3eb700dd6882754
Paul Chyz <paulhchyz@gmail.com>
2019-07-24 13:37:45 -0700

    Fixing dependencies path

--- component-scripts/extract-dependencies.sh
+++ component-scripts/extract-dependencies.sh
@@ -4,13 +4,14 @@
 
 #opc_path=($PWD)
 opc_path=(/home/opc)
+dependencies_path=$PWD/../dependencies
 
 # Extract dependencies to designated directories
 echo "Extracting jdk..."
-tar -C $opc_path/java -zxf dependencies/jdk-8u131-linux-x64.tar.gz
+tar -C $opc_path/java -zxf $dependencies_path/jdk-8u131-linux-x64.tar.gz
 echo "Extracting kafka..."
-tar -C $opc_path/kafka -zxf dependencies/kafka_2.12-2.3.0.tgz
+tar -C $opc_path/kafka -zxf $dependencies_path/kafka_2.12-2.3.0.tgz
 echo "Extracting spark..."
-tar -C $opc_path/spark -zxf dependencies/spark-2.2.1-bin-hadoop2.7.tgz
+tar -C $opc_path/spark -zxf $dependencies_path/spark-2.2.1-bin-hadoop2.7.tgz
 echo "Extracting osa..."
-unzip -q dependencies/V978767-01.zip -d $opc_path/osa
+unzip -q $dependencies_path/V978767-01.zip -d $opc_path/osa
